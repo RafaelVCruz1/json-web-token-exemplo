f6dca6029266bd74df7b7b4d8e62b5c2d4ef2c39
RafaelVCruz1 <rafael.cruz3@estudante.ifms.edu.br>
2023-08-31 20:27:49 -0400

    Formatando o documento

--- servidor/index.js
+++ servidor/index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(cookieParser());
@@ -28,68 +28,68 @@ app.use(
   }).unless({ path: ["/usuarios/cadastrar", "/autenticar", "/logar", "/deslogar",] })
 );
 
-app.get('/autenticar', async function(req, res){
+app.get('/autenticar', async function (req, res) {
   res.render('autenticar');
 })
 
-app.get('/usuarios/cadastrar', async function(req, res){
- res.render('cadastrar')
+app.get('/usuarios/cadastrar', async function (req, res) {
+  res.render('cadastrar')
 
 })
 
-app.post('/usuarios/cadastrar', async function(req, res){
+app.post('/usuarios/cadastrar', async function (req, res) {
 
-  if( req.body.senha == req.body.confirmesenha){
-   await usuario.create(req.body);
-   res.redirect("/usuarios/listar")
-    } else{
-     res.status(500).json({mensagem: "não foi possível cadastrar"})
-    }
- })
+  if (req.body.senha == req.body.confirmesenha) {
+    await usuario.create(req.body);
+    res.redirect("/usuarios/listar")
+  } else {
+    res.status(500).json({ mensagem: "não foi possível cadastrar" })
+  }
+})
 
 
 
- app.get('/usuarios/listar', async function(req, res){
-try{
-  const list = await usuario.findAll();
-  res.render('listar', {list})
-  
-}catch(error){
-  console.error(error);
-  res.status(500).send("Erro ao listar")
-}
- })
+app.get('/usuarios/listar', async function (req, res) {
+  try {
+    const list = await usuario.findAll();
+    res.render('listar', { list })
 
- 
-app.get('/', async function(req, res){
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao listar")
+  }
+})
+
+
+app.get('/', async function (req, res) {
   res.render("home")
 })
 
 app.post('/logar', (req, res) => {
-  if(req.body.usuario == "Rafael" && req.body.senha == "123"){
+  if (req.body.usuario == "Rafael" && req.body.senha == "123") {
     const id = 1
-    
+
     const token = jwt.sign({ id }, process.env.SECRET, {
       expiresIn: 300
     })
 
-    res.cookie('token', token, {httpOnly: true});
+    res.cookie('token', token, { httpOnly: true });
     return res.json({
-      usuario: req.body.usuario, 
-      token: token 
+      usuario: req.body.usuario,
+      token: token
     })
   }
-  
-  res.status(500).json({mensagem: "Login Inválido"})
+
+  res.status(500).json({ mensagem: "Login Inválido" })
 })
 
-app.post('/deslogar', function(req, res) {
-  res.cookie('token', null, {httpOnly: true});
+app.post('/deslogar', function (req, res) {
+  res.cookie('token', null, { httpOnly: true });
   return res.json({
     deslogado: true
   })
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('App de Exemplo escutando na porta 3000!')
 });
