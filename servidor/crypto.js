b9f81f01025eeb969068268309fceca657150038
RafaelVCruz1 <rafael.cruz3@estudante.ifms.edu.br>
2023-09-29 15:35:57 -0400

    Create crypto.js

---
+++ servidor/crypto.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+const CHAVE = '98243hj9823h823ht928ht893ht2983h'; 
+const IV = "3284t93498ygt348"; 
+const ALGORITMO = "aes-256-cbc";
+const METODO_CRIPTOGRAFIA = 'hex';
+const METODO_DESCRIPTOGRAFIA = 'hex';
+
+const encrypt = ((text) =>  {
+   let cipher = crypto.createCipheriv(ALGORITMO, CHAVE, IV);
+   let encrypted = cipher.update(text, 'utf8', METODO_CRIPTOGRAFIA);
+   encrypted += cipher.final(METODO_CRIPTOGRAFIA);
+   return encrypted;
+});
+
+const decrypt = ((text) => {
+   let decipher = crypto.createDecipheriv(ALGORITMO, CHAVE, IV);
+   let decrypted = decipher.update(text, METODO_DESCRIPTOGRAFIA, 'utf8');
+   return (decrypted + decipher.final('utf8'));
+});
+
+module.exports = {encrypt, decrypt};
